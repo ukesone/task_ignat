e0d28c04b8cb174a3aef134f8cb98cf0308d417f
SS007 <sergeysevostyanov007@gmail.com>
2020-04-17 19:59:29 +0400

    Updates

---
+++ precache-manifest.3e8f09c4d4b433e5f7afbb38f0c2f7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44f451f023e00b29c265224927777c5",
+    "url": "/task_ignat/index.html"
+  },
+  {
+    "revision": "1b21bc5fbc49875aa785",
+    "url": "/task_ignat/static/css/main.722b0394.chunk.css"
+  },
+  {
+    "revision": "958971de2c82a9cd413a",
+    "url": "/task_ignat/static/js/2.2a83c311.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task_ignat/static/js/2.2a83c311.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b21bc5fbc49875aa785",
+    "url": "/task_ignat/static/js/main.ee643243.chunk.js"
+  },
+  {
+    "revision": "977cc0c5f59960acded6",
+    "url": "/task_ignat/static/js/runtime-main.5beaceaa.js"
+  }
+]);
